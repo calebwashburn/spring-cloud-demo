15e62b15dbbb6297e5ac744268970cde55b635a4
Caleb Washburn <cwashburn@pivotal.io>
2016-06-22 21:21:15 -0700

    update to consume downstream service using http aka direct

--- restaurant/src/main/java/com/cwashburn/service/Restaurant.java
+++ restaurant/src/main/java/com/cwashburn/service/Restaurant.java
@@ -2,6 +2,8 @@ package com.cwashburn.service;
 
 import java.net.URI;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
@@ -14,6 +16,8 @@ import com.cwashburn.domain.RestaurantMenu;
 
 @Component
 public class Restaurant {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(Restaurant.class);
 
 	@Autowired
 	@LoadBalanced
@@ -23,7 +27,10 @@ public class Restaurant {
 	String name;
 	
 	public RestaurantMenu getMenu() {
+		//if downstream service uses direct use http vs using https if route registration method for url
+		
 		URI uri = UriComponentsBuilder.fromUriString("http://menu/special").build().toUri();
+		LOGGER.info(String.format("Invoking service on url: %s", uri.toString()));
 		MenuSpecial menu = rest.getForObject(uri, MenuSpecial.class);
 		return new RestaurantMenu(menu.getSpecial(), name);
 	}
